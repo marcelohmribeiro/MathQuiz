69c1d174f07ff25798bfeebffd15836eef916b92
Eryck Mike <eryckmike@MacBook-Air-de-Eryck.local>
2025-04-25 16:01:43 -0300

    atualizei o css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,19 +30,21 @@ function Home() {
     }
 
     return (
-        <div className="flex items-center justify-center h-screen bg-gradient-to-br from-[#050805] to-[#0c1c0c] text-[#c7f14f] px-4">
-            <div className="border border-[#c7f14f] bg-[#132513]/80 backdrop-blur-sm rounded-2xl w-full max-w-sm p-8 text-center shadow-lg shadow-[#c7f14f]/40">
-                <h1 className="text-4xl font-extrabold uppercase mb-10 tracking-widest text-[#f4ff9b]">
-                    Quebra Código
+        
+        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-[#050805] to-[#0c1c0c] text-[#c7f14f] px-4">
+            <h1 className="text-6xl font-sixtyfour uppercase mb-10 tracking-widest text-[#B1E325] font-[Sixtyfour,_sans-serif]">
+                    Quebra Codigo
                 </h1>
-                <label htmlFor="difficulty" className="block mb-2 text-lg text-[#f4ff9b] font-medium">
+            <div className="border border-[#c7f14f] bg-[#132513]/80 backdrop-blur-sm rounded-2xl w-full max-w-sm p-8 text-center shadow-lg shadow-[#c7f14f]/40">
+                
+                <label htmlFor="difficulty" className="block mb-2 text-3xl text-[#F7FCE9] font-['Jersey_15',_sans-serif]">
                     Escolha a dificuldade
                 </label>
                 <select
                     name="difficulty"
                     value={selectedDifficulty}
                     onChange={handleDifficultyChange}
-                    className="w-full p-3 mb-8 bg-[#1e2b1e] border border-[#c7f14f] text-[#f4ff9b] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#c7f14f] transition-all duration-200"
+                    className=" appearance-none w-full p-3 mb-8 bg-[#6A8816] border-[3px] border-[#c7f14f] text-[#F7FCE9] font-['Jersey_15',_sans-serif] text-3xl px-2 py-1 rounded-xl focus:outline-none focus:ring-2 focus:ring-[#c7f14f] transition-all duration-200 select-centers"
                 >
                     {difficulties.map((difficulty) => (
                         <option key={difficulty.value} value={difficulty.value}>
@@ -52,9 +54,9 @@ function Home() {
                 </select>
                 <button
                     onClick={handlePlay}
-                    className="bg-[#7ba64c] hover:bg-[#91c659] cursor-pointer active:scale-95 transition-all duration-200 text-black font-bold text-lg px-6 py-3 rounded-xl w-full shadow-lg"
+                    className="bg-[#6A8816] border-[3px] border-[#C0E850] rounded-md hover:bg-[#91c659] cursor-pointer active:scale-95 transition-all duration-200 text-[#F7FCE9] font-['Jersey_15',_sans-serif] text-3xl px-2 py-1 rounded-xl w-full shadow-lg"
                 >
-                    🎮 Jogar
+                    Jogar
                 </button>
             </div>
         </div>
